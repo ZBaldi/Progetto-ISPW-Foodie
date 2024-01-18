098e8673a3f71dbc2df01f18dca28122461d1526
ZValerio <118385498+ZValerio@users.noreply.github.com>
2024-01-18 19:47:55 +0100

    Delete Progetto directory

--- Progetto/src/com/foodie/model/CatalogoRicetteChefDao.java
+++
@@ -1,9 +0,0 @@
-package com.foodie.model;
-
-import java.util.ArrayList;
-
-public interface CatalogoRicetteChefDao {
-	public ArrayList<Ricetta> trovaRicetta(Dispensa dispensa,int difficolta);
-	public void aggiungiRicetta(Ricetta ricetta);
-	public void eliminaRicetta(Ricetta ricetta);
-}
