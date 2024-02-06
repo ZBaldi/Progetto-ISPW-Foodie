839b9a24317982bb6e17d8e9204bcb162676d2f3
ZValerio <118385498+ZValerio@users.noreply.github.com>
2024-02-06 23:09:34 +0100

    Delete src directory

--- src/com/foodie/model/CatalogoAlimentiDao.java
+++
@@ -1,9 +0,0 @@
-package com.foodie.model;
-
-import java.util.ArrayList;
-
-public interface CatalogoAlimentiDao {  //DAO PER GLI ALIMENTI
-	
-	public ArrayList<Alimento> trovaAlimenti(String nome);
-	
-}
