34880b06db7efe5f95285bccebbe71f8a2fdf9d6
ZValerio <118385498+ZValerio@users.noreply.github.com>
2024-02-09 13:29:17 +0100

    Add files via upload

--- Test/com/foodie/test/TestDispensa.java
+++ Test/com/foodie/test/TestDispensa.java
@@ -10,12 +10,15 @@ import org.junit.Test;
 import com.foodie.model.Alimento;
 import com.foodie.model.Dispensa;
 
-public class TestDispensa {
+public class TestDispensa {    //VALERIO BALDAZZI
+	
+	private static final String NOME1= "Pizza";
+	private static final String NOME2= "Pasta";
 	
 	@Before
 	public void riempiDispensa() {
 		Dispensa dispensa = Dispensa.ottieniIstanza();
-		Alimento alimentoProva= new Alimento("Pizza");
+		Alimento alimentoProva= new Alimento(NOME1);
 		dispensa.aggiungiAlimento(alimentoProva);
 	}
 	
@@ -24,18 +27,18 @@ public class TestDispensa {
 	public void testAggiungiAlimentoNonPresente() {
 		Dispensa dispensa= Dispensa.ottieniIstanza();
 		int oldSize= dispensa.getAlimenti().size();
-		Alimento alimentoProva= new Alimento("Pasta");
+		Alimento alimentoProva= new Alimento(NOME2);
 		dispensa.aggiungiAlimento(alimentoProva);
 		assertTrue(dispensa.getAlimenti().contains(alimentoProva));  //CONTROLLO SE L'ALIMENTO E' STATO AGGIUNTO
 		assertEquals(dispensa.getAlimenti().size(), oldSize+1);
-		dispensa.eliminaAlimento(alimentoProva);  //COSI LA RIPORTO ALLO STATO INIZIALE PER FARE GLI ALTRI TEST
+		dispensa.eliminaAlimento(alimentoProva);  //COSI' LA RIPORTO ALLO STATO INIZIALE PER FARE GLI ALTRI TEST
 	}
 	
 	@Test
 	public void testAggiungiAlimentoPresente() {
 		Dispensa dispensa= Dispensa.ottieniIstanza();
 		int oldSize= dispensa.getAlimenti().size();
-		Alimento alimentoProva= new Alimento("Pizza");
+		Alimento alimentoProva= new Alimento(NOME1);
 		dispensa.aggiungiAlimento(alimentoProva);
 		assertEquals(dispensa.getAlimenti().size(),oldSize);   //CONTROLLO SE LA DISPENSA IN DIMENSIONI E' RIMASTA INVARIATA
 	}
@@ -44,17 +47,17 @@ public class TestDispensa {
 	public void testEliminaAlimentoNonPresente() {
 		Dispensa dispensa= Dispensa.ottieniIstanza();
 		int oldSize= dispensa.getAlimenti().size();
-		Alimento alimentoProva= new Alimento("Pasta");
+		Alimento alimentoProva= new Alimento(NOME2);
 		dispensa.eliminaAlimento(alimentoProva);
-		assertFalse(dispensa.getAlimenti().contains(alimentoProva));
-		assertEquals(dispensa.getAlimenti().size(),oldSize);
+		assertFalse(dispensa.getAlimenti().contains(alimentoProva));  //CONTROLLO CHE NON E' STATO AGGIUNTO 
+		assertEquals(dispensa.getAlimenti().size(),oldSize);  //CONTROLLO CHE LA DIMENSIONE E' RIMASTA INVARIATA
 	}
 	
 	@Test
 	public void testEliminaAlimentoPresente() {
 		Dispensa dispensa= Dispensa.ottieniIstanza();
 		int oldSize= dispensa.getAlimenti().size();
-		Alimento alimentoProva= new Alimento("Pizza");
+		Alimento alimentoProva= new Alimento(NOME1);
 		dispensa.eliminaAlimento(alimentoProva);
 		assertFalse(dispensa.getAlimenti().contains(alimentoProva));
 		assertEquals(dispensa.getAlimenti().size(),oldSize-1);
